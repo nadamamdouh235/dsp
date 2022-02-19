0cf820b44ce80cce17e33b1c3193e2c75ac35c9e
nadamamdouh235 <42315079+nadamamdouh235@users.noreply.github.com>
2022-02-19 04:13:18 +0200

    Add files via upload

---
+++ Digital Filter Design/layout.js
@@ -0,0 +1,27 @@
+let menuButton = document.querySelector(".button-menu");
+let container = document.querySelector(".container");
+let pageContent = document.querySelector(".page-content");
+let responsiveBreakpoint = 991;
+let title = document.querySelector(".site-title")
+
+if (window.innerWidth <= responsiveBreakpoint) {
+  container.classList.add("nav-closed");
+}
+
+menuButton.addEventListener("click", function () {
+  title.classList.toggle("hidden")
+  container.classList.toggle("nav-closed");
+});
+
+pageContent.addEventListener("click", function () {
+  if (window.innerWidth <= responsiveBreakpoint) {
+    container.classList.add("nav-closed");
+  }
+});
+
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > responsiveBreakpoint) {
+    container.classList.remove("nav-closed");
+  }
+});
